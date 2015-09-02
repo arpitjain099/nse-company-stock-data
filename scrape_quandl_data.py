30414ee01cfddeaaeccba00b2a79c6f0869c93bb
Arpit Jain <arpitjain099@gmail.com>
2015-09-02 20:50:49 +0530

    pushing files

--- scrape_quandl_data.py
+++ scrape_quandl_data.py
@@ -15,14 +15,25 @@
 import json
 import Quandl
 import os
+import sys
+import codecs
+fi=codecs.open("error.log","a+",encoding="utf-8")
 with open('dump.json') as data_file:    
     data = json.load(data_file)
     print len(data['datasets'])
     for i in data['datasets']:
     	temp=i['database_code']+'/'+i['dataset_code']
     	print i['name']
+
     	#mydata = Quandl.get(temp, authtoken="JVbNrP-_zsnbC7i6yCUM")
     	if not os.path.isfile('dump/'+i['dataset_code']+'_'+i['name']+".csv"):
-		print i['database_code']+"/"+i['dataset_code']
-	    	mydata = Quandl.get(i['database_code']+"/"+i['dataset_code'], authtoken="JVbNrP-_zsnbC7i6yCUM")
-    		mydata.to_csv('dump/'+i['dataset_code']+'_'+i['name']+".csv")
+            #dada=Quandl.get(i['database_code']+"/"+"20MICRONS", authtoken="JVbNrP-_zsnbC7i6yCUM")
+            #print dada
+            #print i['database_code']+"/"+i['dataset_code']
+            try:
+                mydata = Quandl.get(i['database_code']+"/"+i['dataset_code'], authtoken="gNHGL_H-4X9BPuLvnrBC")
+                mydata.to_csv('dump/'+i['dataset_code']+'_'+i['name']+".csv")
+            except:
+                print "error in "+i['database_code']+"/"+i['dataset_code']
+                fi.write(i['database_code']+"/"+i['dataset_code']+"\n")
+fi.close()
